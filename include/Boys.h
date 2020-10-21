3d3c8561b09076153c9747bc2baccaae67f0c039
Piotr Futymski <piotr.futymski@wp.pl>
2020-10-21 15:30:41 +0200

    changed boys implementation to easier

--- include/Boys.h
+++ include/Boys.h
@@ -1,22 +1,10 @@
 #pragma once
-#include <cmath>
 #include "Constants.h"
-#include <map>
-#include <vector>
 
 class BoysCalculator
 {
-static std::map<double, double> boysTab;
-
 public:
-
-	static int silnia(int n);
-	static double boysF1(double x);
 	static double boys(double x);
-	static double boysHighz(int m, double z, int N);
-	static double boysGaussJacobi(int m, double z);
+	static double boysSmall(double x);
+	static double boysBig(double x);
 };
-
-
-
-
